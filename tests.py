62fcdfab3d3f06769818a8b002faa0537b84a870
Alexander Lazoreth <lazoreth@gmail.com>
2019-04-04 13:41:23 +0300

    cardmask fix

--- tests.py
+++ tests.py
@@ -20,9 +20,9 @@ class TestCardMask(unittest.TestCase):
         n1 = random.randint(10**15, 10**16-1)
         n2 = random.randint(10**17, 10**18-1)
         result1 = card_mask(n1)
-        self.assertEqual(result1, f"{str(n1)[0:4]} {str(n1)[-4:]}")
+        self.assertEqual(result1, f"**** **** **** {str(n1)[-4:]}")
         result2 = card_mask(n2)
-        self.assertEqual(result2, f"{str(n2)[0:4]} {str(n2)[-6:-2]} {str(n2)[-2:]}")
+        self.assertEqual(result2, f"**** **** **** {str(n2)[-6:-2]} {str(n2)[-2:]}")
         self.assertRaises(ValueError, card_mask, "123323232")
 
 
